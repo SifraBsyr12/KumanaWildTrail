0a7da0b578457646c4adf9d1f866e4721a18ea5b
Sifra_Bsyr12 <sifrabasayar12@gmail.com>
2026-03-18 07:31:02 +0530

    main

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+window.global = window;
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
