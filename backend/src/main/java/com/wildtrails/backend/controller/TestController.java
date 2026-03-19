f8b4df77f93d22cccec38c683ebf1687972fb837
Sifra_Bsyr12 <sifrabasayar12@gmail.com>
2026-03-19 06:52:31 +0530

    TestController

---
+++ backend/src/main/java/com/wildtrails/backend/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.wildtrails.backend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping
+    public String getTestMessage() {
+        return "Hello, this is a test message!";
+    }
+}
