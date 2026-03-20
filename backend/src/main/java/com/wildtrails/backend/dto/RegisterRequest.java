298dc1c52f3c95dd14c04672123c98a0bfd01dbc
Sifra_Bsyr12 <sifrabasayar12@gmail.com>
2026-03-20 17:53:29 +0530

    RegisterRequest

---
+++ backend/src/main/java/com/wildtrails/backend/dto/RegisterRequest.java
@@ -0,0 +1,60 @@
+package com.wildtrails.backend.dto;
+
+import com.wildtrails.backend.entity.Role;
+
+public class RegisterRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+    private String vehicleType;
+    private int seatingCapacity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public int getSeatingCapacity() {
+        return seatingCapacity;
+    }
+
+    public void setSeatingCapacity(int seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+    }
+}
