3117828121037321af2e28f950c299406fed9955
Sifra_Bsyr12 <sifrabasayar12@gmail.com>
2026-03-20 17:57:40 +0530

    SightingTimeDTO

---
+++ backend/src/main/java/com/wildtrails/backend/dto/SightingTimeDTO.java
@@ -0,0 +1,21 @@
+package com.wildtrails.backend.dto;
+
+import java.time.LocalDateTime;
+
+public class SightingTimeDTO {
+    private String animalName;
+    private LocalDateTime dateTime;
+
+    public SightingTimeDTO(String animalName, LocalDateTime dateTime) {
+        this.animalName = animalName;
+        this.dateTime = dateTime;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+}
