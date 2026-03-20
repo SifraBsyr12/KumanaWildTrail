58fffa539bff03a8dd42cfe858390705faa3cdb1
Sifra_Bsyr12 <sifrabasayar12@gmail.com>
2026-03-20 17:43:51 +0530

    AnimalHotspotDTO

---
+++ backend/src/main/java/com/wildtrails/backend/dto/AnimalHotspotDTO.java
@@ -0,0 +1,12 @@
+package com.wildtrails.backend.dto;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
+@Data
+public class AnimalHotspotDTO {
+    private String animalName;
+    private double lat;
+    private double lng;
+    private long sightingsCount;
+}
