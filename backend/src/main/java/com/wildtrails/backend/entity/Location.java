f5ef60c65cb69955f0eba811791d9818d568944a
Sifra_Bsyr12 <sifrabasayar12@gmail.com>
2026-03-22 10:42:57 +0530

    Location

---
+++ backend/src/main/java/com/wildtrails/backend/entity/Location.java
@@ -0,0 +1,15 @@
+package com.wildtrails.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class Location {
+    @Column(nullable = true)
+    private Double lat;
+
+    @Column(nullable = true)
+    private Double lng;
+}
