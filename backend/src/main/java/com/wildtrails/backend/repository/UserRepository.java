b8c273d9a8bdb7e8da01295b90a7bc402639846e
Sifra_Bsyr12 <sifrabasayar12@gmail.com>
2026-03-24 08:39:17 +0530

    UserRepository

---
+++ backend/src/main/java/com/wildtrails/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.wildtrails.backend.repository;
+import com.wildtrails.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
