176915efba197f613578f14a7a97455f7452bc95
Sifra_Bsyr12 <sifrabasayar12@gmail.com>
2026-03-26 08:07:30 +0530

    FirebaseTokenService

---
+++ backend/src/main/java/com/wildtrails/backend/service/FirebaseTokenService.java
@@ -0,0 +1,14 @@
+package com.wildtrails.backend.service;
+
+import org.springframework.stereotype.Service;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+@Service
+public class FirebaseTokenService {
+      public FirebaseToken verifyToken(String idToken) throws FirebaseAuthException {
+        return FirebaseAuth.getInstance().verifyIdToken(idToken);
+    }
+}
